76398d107ab58a5288a5b81ec878235e215dac5b
Rvannest <121450248+Rvannest@users.noreply.github.com>
2023-12-10 11:41:55 -0500

    Update Apriori.py

--- Apriori.py
+++ Apriori.py
@@ -1,52 +1,69 @@
 import time
 from collections import defaultdict
 
-baskets = []
+each_baskets = []
 
-data_file_path = ""
+text_path = "C:\\Users\\R\\vscodework\\4250\\Project1\\retail.txt"
 
-with open(data_file_path, 'r') as file:
-    for line in file:
-        single_basket = [int(item) for item in line.strip().split()]
-        baskets.append(single_basket)
+# chunks and thresholds
+set_thresholds = [0.01, 0.05, 0.10]  #1%, 5%, 10% chunks
+set_sizes_of_dataset = [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]  #10%, 20%, 50%, 100% data
 
-# A-Priori function
-def apriori(baskets, support_threshold):
-    singleton_counts = defaultdict(int)
-    for basket in baskets:
+
+with open(text_path, 'r') as text:
+    for line in text:
+        create_singlebasket = [int(item) for item in line.strip().split()]
+        each_baskets.append(create_singlebasket)
+
+
+#A-Priori function
+def apriori(each_baskets, supp_threshold):
+
+    single_itemcount = defaultdict(int)
+    for basket in each_baskets:
         for item in basket:
-            singleton_counts[item] += 1
+            single_itemcount[item] += 1
 
-    frequent_singletons = {k for k, v in singleton_counts.items() if v >= support_threshold}
+    #filter single if above threshold
+    frequent_single_items = set()
+    for k, v in single_itemcount.items():
+        if v >= supp_threshold:
+            frequent_single_items.add(k)
 
-    pair_counts = defaultdict(int)
-    for basket in baskets:
-        basket_set = set(basket) & frequent_singletons
-        for i, item_i in enumerate(basket_set):
-            for item_j in list(basket_set)[i+1:]:
+    #count pairs
+    count_each_pairs = defaultdict(int)
+    for basket in each_baskets:
+        set_basket = set(basket) & frequent_single_items
+        for i, item_i in enumerate(set_basket):
+            for item_j in list(set_basket)[i+1:]:
                 if item_i < item_j:
-                    pair_counts[(item_i, item_j)] += 1
+                    count_each_pairs[(item_i, item_j)] += 1
                 else:
-                    pair_counts[(item_j, item_i)] += 1
+                    count_each_pairs[(item_j, item_i)] += 1
 
-    frequent_pairs = {k for k, v in pair_counts.items() if v >= support_threshold}
+    #get frequent pairs
+    frequent_pair_items = set()
+    for k, v in count_each_pairs.items():
+        if v >= supp_threshold:
+            frequent_pair_items.add(k)
 
-    return frequent_pairs
+    return frequent_pair_items
 
-chunk_sizes = [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]  # 10%, 20%, 50%, 100% of the data
-thresholds = [1, 5, 10]  # 1%, 5%, 10% thresholds
 
-for support_threshold_percentage in thresholds:
-    for chunk_size in chunk_sizes:
-        support_threshold = (len(baskets) * support_threshold_percentage) / 100
+#loop for thresholds and chunks
+for support_percentage in set_thresholds:
+    for chunk_size in set_sizes_of_dataset:
         
-        baskets_chunk = baskets[:int(len(baskets) * chunk_size)]
+        supp_threshold = (len(each_baskets) * support_percentage)
         
-        start_time = time.time()
+        select_basket_chunk = each_baskets[:int(len(each_baskets) * chunk_size)]
         
-        frequent_itemsets = apriori(baskets_chunk, support_threshold)
+        # measure time
+        start_timer = time.time()
         
-        execution_time = (time.time() - start_time) * 1000
-        print(f"Execution Time: {execution_time:.2f} ms with a chunk size of {chunk_size:.2f} and {support_threshold_percentage}% threshold")
+        find_frequent_itemsets = apriori(select_basket_chunk, supp_threshold)
         
-        print(f"Frequent Itemsets: {frequent_itemsets}\n")
+        print(f"Frequent Itemsets: {find_frequent_itemsets}")
+
+        execution_time = (time.time() - start_timer) * 1000
+        print(f"Execution Time: {execution_time:.2f} ms --- Chunk size is {chunk_size:.2f} --- Support threshold is {support_percentage*100}%\n")
